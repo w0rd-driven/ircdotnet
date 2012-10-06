b89a14621b289b34ef3414899653928ed39b0e54
Jeremy Brayton <jeremy@braytonium.com>
2012-10-06 09:36:37 -0400

    Added one-click build system stolen from autofac and perfected through use. This sets the stage for the WindowsPhone branch

---
+++ Build.cmd
@@ -0,0 +1,3 @@
+@echo off
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild IrcDotNet.build %*
+pause
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IRC.NET")]
+[assembly: AssemblyCopyright("Copyright © Alex Regueiro 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+//[assembly: CLSCompliant(true)]
