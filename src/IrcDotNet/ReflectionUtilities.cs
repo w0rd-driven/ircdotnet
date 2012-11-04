ee8cc9fab528e6f132bfc213d42eebae0a36c56a
Jeremy Brayton <jeremy@braytonium.com>
2012-11-04 00:17:24 -0400

    More CRLF
    Fixed ReflectionUtilities. Will likely need to revisit this as GetMethods may not map to GetRuntimeMethods in a meaningful way

--- src/IrcDotNet/ReflectionUtilities.cs
+++ src/IrcDotNet/ReflectionUtilities.cs
@@ -15,15 +15,15 @@ namespace IrcDotNet
             where TDelegate : class
         {
             // Find all methods in class that are marked by one or more instances of given attribute.
-            var messageProcessorsMethods = obj.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic
-                | BindingFlags.Public);
+            var messageProcessorsMethods = obj.GetType().GetRuntimeMethods();//.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic
+            //| BindingFlags.Public);
             foreach (var methodInfo in messageProcessorsMethods)
             {
                 var methodAttributes = (TAttribute[])methodInfo.GetCustomAttributes(
                     typeof(TAttribute), true);
                 if (methodAttributes.Length > 0)
                 {
-                    var methodDelegate = (TDelegate)(object)Delegate.CreateDelegate(typeof(TDelegate), obj, methodInfo);
+                    var methodDelegate = (TDelegate)(object)methodInfo.CreateDelegate(typeof(TDelegate), obj);
 
                     // Get each attribute applied to method.
                     foreach (var attribute in methodAttributes)
